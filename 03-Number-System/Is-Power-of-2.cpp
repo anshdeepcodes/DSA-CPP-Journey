79d71a9577fd1162421fcb94ac8c0cc51fdb4244
Anshdeep <anshdeep.codes@gmail.com>
2026-02-19 18:54:06 +0530

    fix: resolve variable initialization bug in bitwise check

--- 03-Number-System/Is-Power-of-2.cpp
+++ 03-Number-System/Is-Power-of-2.cpp
@@ -6,8 +6,8 @@ int main() {
     cin >> n;
 
     // A number is power of 2 if it has only one set bit
-    if (num > 0 && (num & (num - 1)) == 0) {
-        cout << num << " is power of 2" << endl;
+    if (n > 0 && (n & (n - 1)) == 0) {
+        cout << n << " is power of 2" << endl;
     } else {
         cout << "Not a power of 2";
     }
