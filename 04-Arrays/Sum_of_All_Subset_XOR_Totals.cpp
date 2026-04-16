ebbde124843077a7d9c287d201408a67974b45f0
Anshdeep <anshdeep.codes@gmail.com>
2026-04-16 22:29:30 +0530

    Add: Subset XOR Sum using Bit Masking

---
+++ 04-Arrays/Sum_of_All_Subset_XOR_Totals.cpp
@@ -0,0 +1,32 @@
+/*
+Problem: Sum of All Subset XOR Totals
+Link: https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+Approach: Bit Masking (Generating all 2^n subsets)
+Time Complexity: O(n * 2^n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = 0;
+
+        // Total subsets are 2^n. We use bitwise shift (1 << n) for this.
+        int totalSubsets = 1 << n;
+
+        // Iterate from 0 to 2^n - 1 to represent each subset
+        for(int i = 0; i < totalSubsets; i++) {
+            int currentXOR = 0;
+
+            for(int j = 0; j < n; j++) {
+                // If the j-th bit of i is set, include nums[j] in current XOR
+                if(i & (1 << j)) {
+                    currentXOR ^= nums[j];
+                }
+            }
+            totalSum += currentXOR;
+        }
+        return totalSum;
+    }
+};
